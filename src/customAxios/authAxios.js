31bf52e051254937e3573191aae2cd67ab3c4238
Yash Maurya <yashmaurya2109@gmail.com>
2024-07-30 13:34:30 +0530

    Changed url

--- src/customAxios/authAxios.js
+++ src/customAxios/authAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://mnnlr-backend.onrender.com'
- const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://mnnlr-backend.onrender.com'
+//  const BASE_URL = 'http://localhost:5000'
 
 export default axios.create({
     baseURL: BASE_URL,
