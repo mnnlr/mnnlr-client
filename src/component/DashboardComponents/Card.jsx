6aed22a97790edcee0b69e19fc26095569c344da
mnnlr <167679533+mnnlr@users.noreply.github.com>
2025-01-15 11:35:59 +0530

    Update Card.jsx

--- src/component/DashboardComponents/Card.jsx
+++ src/component/DashboardComponents/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ title, amount, percentage, icon: Icon, bgColorFrom, bgColorTo }) => {
   return (
-    <div className="flex items-center p-6 bg-white rounded-lg shadow-md cursor-pointer" style = {{width: '250px', margin : "20px"}}>
+    <div className="flex items-center p-4 bg-white rounded-lg shadow-md cursor-pointer" style = {{width: '250px', margin : "20px"}}>
       <div className="flex-grow">
         <p className="text-gray-500 font-bold text-sm">{title}</p>
         <div className="flex items-baseline">
